59216d36a96a7917a89c48718fade63d456c57cf
rahelalula <89055683+rahelalula@users.noreply.github.com>
2021-08-21 22:35:08 -0500

    Java project from eclipse + calculator

---
+++ Helloworld/src/com/RahelA/Gitdemo.java
@@ -0,0 +1,15 @@
+package com.RahelA;
+
+public class Gitdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to Java world");
+		System.out.println("craft-common-10");
+		System.out.println("class of August");
+		
+		
+	}
+
+}
